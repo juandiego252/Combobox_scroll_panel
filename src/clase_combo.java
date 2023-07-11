a34862ff91f5245026215a5cf7ef9453bb2952d3
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-07-11 09:52:53 -0500

    Agregado scroll bar

--- src/clase_combo.java
+++ src/clase_combo.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 
 public class clase_combo {
     private JTextField textField1;
@@ -8,6 +10,9 @@ public class clase_combo {
     private JComboBox comboBox1;
     private JLabel TextDisplay1;
     private JPanel root;
+    private JLabel edad3;
+    private JScrollBar scrollBar1;
+    private JLabel edad_dato;
 
     String aux = null;
     public clase_combo() {
@@ -17,7 +22,14 @@ public class clase_combo {
 
             }
         });
+        scrollBar1.addAdjustmentListener(new AdjustmentListener() {
+            @Override
+            public void adjustmentValueChanged(AdjustmentEvent e) {
+                edad_dato.setText(String.valueOf(scrollBar1.getValue()));
+            }
+        });
     }
+
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("clase_combo");
