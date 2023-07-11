e0a5f21e8eb77bb48e9963b9910c793566c3286e
Juan Diego <juandiegojd252@gmail.com>
2023-07-11 09:41:07 -0500

    Initial commit

---
+++ src/clase_combo.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class clase_combo {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JLabel TextDisplay1;
+    private JPanel root;
+
+    public clase_combo() {
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("clase_combo");
+        frame.setContentPane(new clase_combo().root);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
